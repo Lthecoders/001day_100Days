7bc5df7e8c3c2c4f2f1947d27a134314552215eb
Nikhil Gaikwad <162660405+Lthecoders@users.noreply.github.com>
2024-04-27 12:43:27 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,98 @@
+# print("hello replit")
+# print('hello repli')
+
+"in print statement everything which goes inside the double quotes is considered as a "
+"string"
+
+
+# print("Well we")
+# print("just use more lines")
+# print("of code")
+
+
+# print("""😎😎😎😎Anything that starts
+# with three quotes, and ends
+# in three quotes can span
+# many lines and even contain " symbols
+# within it without freaking anything out!😊😊😊👌""")
+
+"print is case sensitive"
+
+"------------day 1 challenge----------"
+"everthing in just one sing print function"
+
+print("""Nikhil
+April 24-04-2# I am signing up for Replit's 100 days of Python challenge!
+I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happend to be. No excuses for not coding from the middle of a field!
+I am feeling 😊
+you can follow my progress at replit.com/@nikhilgaikwad9
+""")
+
+
+
+
+
+
+
+
+# print("= MUSIC+ =")
+# print('> Songs')
+# print('> Albums')
+# print("> Artists")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
