98bcceb1513b0feccc41c4e8c87f6fcada074f88
Nikhil Gaikwad <162660405+Lthecoders@users.noreply.github.com>
2024-05-02 21:16:34 +0530

    Create notes.py
    this is where i had will code what i had learned during videos

---
+++ notes.py
@@ -0,0 +1,17 @@
+# print("hello replit")
+# print('hello repli')
+
+"in print statement everything which goes inside the double quotes is considered as a "
+"string"
+
+
+# print("Well we")
+# print("just use more lines")
+# print("of code")
+
+
+# print("""😎😎😎😎Anything that starts
+# with three quotes, and ends
+# in three quotes can span
+# many lines and even contain " symbols
+# within it without freaking anything out!😊😊😊👌""")
